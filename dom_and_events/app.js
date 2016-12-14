a69cbafc81575fb62c28c961033ba89841ac41cb
Angela Geronimo <angelageroniimo@gmail.com>
2016-12-13 21:36:07 -1000

    tried to take notes

---
+++ dom_and_events/app.js
@@ -0,0 +1,24 @@
+/*
+ Angela Geronimo
+ Dec 13, 2016
+*/
+
+
+/*
+
+Questions: 
+
+1. What is the DOM?
+
+2. What is a Javascript event?
+
+3. Name and describe 2 types of Javascript Events.
+
+4. How do you add a js externally to your index.html page?
+
+5. How do you change a class or id name in js? How do you change the background color 
+of an element using javascript?
+
+	(112111)
+*/
+
